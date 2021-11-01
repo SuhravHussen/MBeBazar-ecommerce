0c74c995df74e83b5de0b23862d1534ec8dfb711
SuhravHussen <suhravshan@gmail.com>
2021-11-01 13:01:58 +0600

    materialUi setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
-import type { AppProps } from 'next/app';
-import '../styles/globals.scss';
+/* eslint-disable react/forbid-prop-types */
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import createEmotionCache from '../src/createEmotionCache';
+import theme from '../src/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props: any) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>My page</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
-export default MyApp;
+
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    // eslint-disable-next-line react/require-default-props
+    emotionCache: PropTypes.object,
+    pageProps: PropTypes.object.isRequired,
+};
