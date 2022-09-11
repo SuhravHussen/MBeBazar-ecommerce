2189e1a78c18538ad107ad98681106d74719a298
SuhravHussen <suhravshan@gmail.com>
2022-09-11 18:04:31 +0600

    updated next config

--- Client/next.config.js
+++ Client/next.config.js
@@ -2,17 +2,25 @@
 const path = require('path');
 
 module.exports = {
-    reactStrictMode: true,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-        prependData: `@import "abstracts/variables.scss";
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "abstracts/variables.scss";
         @import "abstracts/mixins.scss";
         `,
-    },
-    images: {
-        domains: ['res.cloudinary.com'],
-    },
-    env: {
-        BASE_URL: process.env.BASE_URL,
-    },
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
