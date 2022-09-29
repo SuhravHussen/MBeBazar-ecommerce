320b08f63b9129773fea386e01fca7e1439e63c5
SuhravHussen <suhravshan@gmail.com>
2022-09-29 14:28:42 +0600

    pwa stopped , bug

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
-const withPWA  = require("next-pwa");
-module.exports = WithPWA(
-  {
+
+module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -24,10 +23,39 @@ module.exports = WithPWA(
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
 };
-)
+
+
+// /** @type {import('next').NextConfig} */
+// const path = require('path');
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//    pwa:{
+//     dest:"public",
+//     register:true,
+//     skipWaiting:true,
+//     disable:process.env.NODE_ENV === 'development'
+//   },
+//   reactStrictMode: true,
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//     prependData: `@import "abstracts/variables.scss";
+//         @import "abstracts/mixins.scss";
+//         `,
+//   },
+//   images: {
+//     domains: ['res.cloudinary.com'],
+//   },
+//   env: {
+//     BASE_URL: process.env.BASE_URL,
+//     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+//     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+//     FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+//     FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
+//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+//     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+//     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+//     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+//   },
+// });
